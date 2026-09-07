df7fea609411f63049596841793fbb19b36a7bb6
Waku waku waku <finetouri0113@i.softbank.jp>
2026-09-07 11:33:55 +0900

    2

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ var context = paper.getContext('2d');
 // Canvasの幅と高さを指定
 paper.width = 400;
 paper.height = 200;
+ // スタイルは描画前に指定
+// 塗りつぶしの色を指定
+context.fillStyle = '#429955';
+// 正方形を描く
+// fillRect(x, y, w, h) ... xyが座標、whは幅と高さ
+context.fillRect(50, 50, 100, 100);
+ // 緑の正方形を描画
+context.fillStyle = '#429955';
+context.fillRect(50, 50, 100, 100);
+// 線を描きます宣言
+context.beginPath();
+// 線をどこから描くか宣言
+context.moveTo(240,80);
+// 線をどこまで描くか宣言
+context.lineTo(320,160);
+// 線の色を指定して、描画じゃ！！！
+context.strokeStyle = '#429955';
+context.stroke();
