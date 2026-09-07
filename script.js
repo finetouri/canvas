e0a2715064ef8ea9aa58eadbbc3878f5c00dd678
Waku waku waku <finetouri0113@i.softbank.jp>
2026-09-07 11:54:59 +0900

    6

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-    // Canvasに指定したidで要素を取得
-var paper = document.getElementById('paper');
-// idから要素を取得できなかったとき
-if (typeof paper.getContext === 'undefined') {
-return;
-}
-// コンテキストを取得
-// 「2d」というのは、描画領域の種類のこと
 var context = paper.getContext('2d');
-// Canvasの幅と高さを指定
-paper.width = 400;
-paper.height = 200;
+paper.width = window.innerWidth;
+paper.height = window.innerHeight;
 context.beginPath();
 context.moveTo(200, 100);
-context.arc(200, 150, 50, 0, Math.PI*2, false);
-context.fillStyle = '#ff0000';
+context.arc(200, 100, 100, 0, Math.PI * 2, false);
+context.fillStyle = 'red';
 context.fill();
+function loop() {
+var paper = document.getElementById('paper');
+if (typeof paper.getContext === 'undefined') {return;}
+var context = paper.getContext('2d');
+paper.width = window.innerWidth;
+paper.height = window.innerHeight;
+var width = paper.width;
+var height = paper.height;
+var x = 0;
+var speed = 5;
+(function render() {
+    context.clearRect(0, 0, width, height);
+    context.beginPath();
+    context.arc(x, 150,50, 0, Math.PI * 2, false);
+    context.fillStyle = 'red';
+    context.fill();
+    if (x > paper.width + 50) {
+        x = -50;
+    } else {
+        x += speed;
+    }
+    requestAnimationFrame(render);
+    })();
+}
+    loop();
